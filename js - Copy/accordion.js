bd2835dfcd05d7d4fb18088294ded0af2dc4a6f2
l7ana <59889633+l7ana@users.noreply.github.com>
2021-05-20 02:50:12 -1000

    whoops

--- js - Copy/accordion.js
+++
@@ -1,8 +0,0 @@
-
-// VANILLA JS EVENT LISTENER FOR MULTIPLE ELEMENTS
-var accordians = document.querySelectorAll('.accordian');
-for (var i = 0, len = accordians.length; i < len; i++) {
-    accordians[i].children[0].addEventListener('click', function () {
-        this.parentElement.classList.toggle('open');
-    });
-}
